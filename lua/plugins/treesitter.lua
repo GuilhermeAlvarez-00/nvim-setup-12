ad7ee3a29820b6478014189f07aaf9baab854a0c
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2026-02-25 20:26:23 -0300

    refactor(treesitter): adjusting for performance

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,21 @@
-require('nvim-treesitter.configs').setup({
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "lua", "vim", "vimdoc", "query", },
+require('nvim-treesitter').setup({
+  ensure_installed = { "javascript", "typescript", "lua", "vim", "vimdoc", "query" },
   sync_install = false,
   auto_install = true,
-  ignore_install = {},
+  ident = {
+    enable = true
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "<Enter>",
+      node_incremental = "<Enter>",
+      scope_incremental = false,
+      node_decremental = "<BS>",
+    },
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
   },
-}
-)
+})
